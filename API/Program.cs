8c7a2d00dc54d65b9901455b1af6481c5508f853
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-08-20 21:37:16 +0700

    ch04-009 Adding AutoMapper to the project

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -15,6 +16,7 @@ builder.Services.AddCors();
 builder.Services.AddMediatR(
     x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>()
 );
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
    
 
 var app = builder.Build();
--- Application/Activities/Commands/EditActivity.cs
+++ Application/Activities/Commands/EditActivity.cs
@@ -1,4 +1,5 @@
 using System;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -12,13 +13,13 @@ public class EditActivity
         public required Activity Activity { get; set; }
     }
 
-    public class Handler(AppDbContext context) : IRequestHandler<Command>
+    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command>
     {
         public async Task Handle(Command request, CancellationToken cancellationToken)
         {
             var existingActivity = await context.Activities.FindAsync([request.Activity.Id], cancellationToken)
                     ?? throw new Exception("Cannot find activity");
-            existingActivity.Title = request.Activity.Title;
+            mapper.Map(request.Activity, existingActivity);
             await context.SaveChangesAsync(cancellationToken);
         }
     }
