26d3273e04cfd2276bc51b1efbd5c79e4bc27d3e
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-08-20 21:03:26 +0700

    ch04-007 Adding a Mediator handler for Creating an Activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,10 +1,7 @@
-using System;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Persistence;
-using Microsoft.EntityFrameworkCore;
-using MediatR;
 using Application.Activities.Queries;
+using Application.Activities.Commands;
 
 namespace API.Controllers;
 
@@ -19,7 +16,13 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        
+
         return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command{ Activity = activity });
     }
 }
