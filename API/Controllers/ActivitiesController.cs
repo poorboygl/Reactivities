299a2e65f86b49d46c576ce2597d6c6ad1cc7367
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-08-20 20:38:48 +0700

    ch04-005 Adding a Mediator handler for the Details

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -19,8 +19,7 @@ public class ActivitiesController(AppDbContext context, IMediator mediator) : Ba
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        var activity = await context.Activities.FindAsync(id);
-        if (activity == null) return NotFound();
-        return activity;
+        
+        return await mediator.Send(new GetActivityDetails.Query { Id = id });
     }
 }
