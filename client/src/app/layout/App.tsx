3cef68783c8c940cc17bd5e179dfb80ea591193c
nguyenvu-laptop <nguye@LAPTOP>
2025-08-23 09:36:04 +0700

    ch05-008 Selecting an individual activity to view

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -6,18 +6,33 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined)
 
   useEffect(() =>{
     axios.get<Activity[]>('https://localhost:5001/api/activities')
       .then(response => setActivities(response.data))
       return () => {}
   }, [])
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.find(activity => activity.id === id))  
+  }
+
+  const handleCancelSelectActivity = () => {
+    setSelectedActivity(undefined)
+  }
+
   return (
     <Box sx={{ bgcolor: '#eeeeee'}}>
       <CssBaseline />
       <NavBar />
       <Container maxWidth='xl' sx={{ mt: 3}}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard 
+          activities={activities}
+          selectActivity={handleSelectActivity} 
+          cancelSelectActivity={handleCancelSelectActivity}
+          selectedActivity={selectedActivity}
+        />
       </Container>
     </Box>
 
--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -2,9 +2,10 @@ import { Button, Card, CardContent, Chip, Typography } from "@mui/material"
 
 type Props = {
   activity: Activity
+  selectActivity : (id: string) => void
 }
 
-export default function ActivityCard({ activity }: Props) {
+export default function ActivityCard({ activity, selectActivity }: Props) {
   return (
     <Card sx={{borderRadius: 3}}>
         <CardContent>
@@ -15,7 +16,7 @@ export default function ActivityCard({ activity }: Props) {
         </CardContent>
         <CardContent sx={{display: 'flex', justifyContent: 'space-between', pd: 2}}>
             <Chip label={activity.category} variant="outlined"></Chip>
-            <Button size='medium' variant="contained">View</Button>
+            <Button onClick={()=> {selectActivity(activity.id)}} size='medium' variant="contained">View</Button>
         </CardContent>
     </Card>
   )
