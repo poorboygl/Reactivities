2c04b1eb867138b51fb6c7b581e57cd0b98736bf
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-08-25 21:55:53 +0700

    ch07-006 Fetching an individual activity from the API

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,7 +1,8 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 
-export const useActivities = () => {
+
+export const useActivities = (id? : string) => {
   const queryClient = useQueryClient();
   
   const { data: activities, isPending } = useQuery({
@@ -11,6 +12,16 @@ export const useActivities = () => {
       return response.data;
     }
   });
+
+  const {data: activity, isLoading: isLoadingActivity} = useQuery({
+      queryKey: ['activities', id],
+      queryFn: async () => {
+        const response = await agent.get<Activity>(`/activities/${id}`);
+        return response.data;
+      },
+      enabled: !!id
+      // !!id means the query will only run if id is truthy
+  })
 
   const updateActivity = useMutation({
     mutationFn: async (activity: Activity) => {
@@ -50,6 +61,8 @@ export const useActivities = () => {
     isPending,
     updateActivity,
     createActivity,
-    deleteActivity
+    deleteActivity,
+    activity,
+    isLoadingActivity
   };
 };
