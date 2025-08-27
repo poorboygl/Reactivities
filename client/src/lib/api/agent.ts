c9bcd0ddfd571c8a0351b5e58a26300f114f7ccc
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-08-27 23:59:38 +0700

    ch09-008 Adding loading state

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
   return new Promise(resolve => setTimeout(resolve, delay));
@@ -8,13 +9,21 @@ const agent = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
 
+agent.interceptors.request.use(config => {
+    // You can modify the request config here
+    store.uiStore.isBusy();
+    return config;
+});
+
 agent.interceptors.response.use( async response => {
     try {
-        await sleep(1000);
+        await sleep(1000)
         return response;
     } catch (error) {
         console.log(error);
         return Promise.reject(error);
+    } finally {
+        store.uiStore.isIdle();
     }
 });
 
