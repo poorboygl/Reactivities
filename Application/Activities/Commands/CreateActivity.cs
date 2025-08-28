73062aaef320469b3789c911b25259a092b564e9
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-08-28 22:50:39 +0700

    ch10-008 Handling API error responses part 3

--- Application/Activities/Commands/CreateActivity.cs
+++ Application/Activities/Commands/CreateActivity.cs
@@ -1,5 +1,6 @@
 using System;
 using Application.Activities.DTOs;
+using Application.Core;
 using AutoMapper;
 using Domain;
 using FluentValidation;
@@ -10,20 +11,22 @@ namespace Application.Activities.Commands;
 
 public class CreateActivity
 {
-    public class Command : IRequest<string>
+    public class Command : IRequest<Result<string>>
     {
         public required CreateActivityDto ActivityDto { get; set; }
     }
 
-    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, string>
+    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, Result<string>>
     {
-        public async Task<string> Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Result<string>> Handle(Command request, CancellationToken cancellationToken)
         {
             var activity = mapper.Map<Activity>(request.ActivityDto);
+            
             context.Activities.Add(activity);
-            await context.SaveChangesAsync(cancellationToken);
 
-            return activity.Id;
+            var result = await context.SaveChangesAsync(cancellationToken) > 0;
+
+            return !result ? Result<string>.Failure("Failed to create activity", 400) : Result<string>.Success(activity.Id);
         }
     }
 }
--- Application/Activities/Commands/DeleteActivity.cs
+++ Application/Activities/Commands/DeleteActivity.cs
@@ -1,4 +1,5 @@
 using System;
+using Application.Core;
 using MediatR;
 using Persistence;
 
@@ -6,20 +7,24 @@ namespace Application.Activities.Commands;
 
 public class DeleteActivity
 {
-    public class Command : IRequest
+    public class Command : IRequest<Result<Unit>>
     {
         public required string Id { get; set; }
     }
 
-    public class Handler(AppDbContext context) : IRequestHandler<Command>
+    public class Handler(AppDbContext context) : IRequestHandler<Command, Result<Unit>>
     {
-        public async Task Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
         {
-            var activity = await context.Activities.FindAsync([request.Id], cancellationToken)
-                ?? throw new Exception("Activity not found");
+            var activity = await context.Activities.FindAsync([request.Id], cancellationToken);
 
-            context.Activities.Remove(activity);
-            await context.SaveChangesAsync(cancellationToken);
+            if (activity == null) return Result<Unit>.Failure("Activity not found", 404);
+
+            context.Remove(activity);
+
+            var result = await context.SaveChangesAsync(cancellationToken) > 0;
+
+            return !result ? Result<Unit>.Failure("Failed to delete activity", 404) : Result<Unit>.Success(Unit.Value);
         }
     }
 }
