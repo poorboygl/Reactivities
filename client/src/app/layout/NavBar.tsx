6bb94b63fbef1dcec86fd44fc777f5b415fe5366
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-02 22:14:05 +0700

    ch13-004 Creating a user menu

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -5,6 +5,7 @@ import MenuItemLink from "../shared/components/MenuItemLink";
 import { useStore } from "../../lib/hooks/useStore";
 import { Observer } from "mobx-react-lite";
 import { useAccount } from "../../lib/hooks/useAccount";
+import UserMenu from "./UserMenu";
 
 export default function NavBar() {
   const { uiStore } = useStore();
@@ -30,9 +31,6 @@ export default function NavBar() {
                 <MenuItemLink  to='/activities'>
                     Activities
                 </MenuItemLink>
-                <MenuItemLink to='/createActivity'>
-                    Create Activity
-                </MenuItemLink>
                 <MenuItemLink to='/counter'>
                     Counter
                 </MenuItemLink>
@@ -42,7 +40,7 @@ export default function NavBar() {
               </Box>
               <Box display='flex' alignItems={'center'}>
                 {currentUser ? (
-                  <Typography> Welcome {currentUser.displayName}</Typography>
+                  <UserMenu />
                 ): (
                   <>
                     <MenuItemLink to='/login'>Login</MenuItemLink>
