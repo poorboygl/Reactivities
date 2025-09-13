301519ddd9b789c399feba265f98edb31b75e1de
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-13 15:51:20 +0700

    ch16-007 Creating a profiles controller

---
+++ API/Controllers/ProfilesController.cs
@@ -0,0 +1,14 @@
+using System;
+using Application.Profiles.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ProfilesController : BaseApiController
+{
+    [HttpPost("add-photo")]
+    public async Task<IActionResult> AddPhoto([FromForm]IFormFile file)
+    {
+        return HandleResult(await Mediator.Send(new AddPhoto.Command { File = file }));
+    }
+}
