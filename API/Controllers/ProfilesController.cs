e39208b3fda1ed24388cea55692d97795a7a2510
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-13 16:21:13 +0700

    ch16-008 Getting a list of photos

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,5 +1,7 @@
 using System;
 using Application.Profiles.Commands;
+using Application.Profiles.Queries;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -7,8 +9,14 @@ namespace API.Controllers;
 public class ProfilesController : BaseApiController
 {
     [HttpPost("add-photo")]
-    public async Task<IActionResult> AddPhoto([FromForm]IFormFile file)
+    public async Task<IActionResult> AddPhoto([FromForm] IFormFile file)
     {
         return HandleResult(await Mediator.Send(new AddPhoto.Command { File = file }));
+    }
+
+    [HttpGet("{userId}/photos")]
+    public async Task<ActionResult<List<Photo>>> GetPhotosForUser(string userId)
+    {
+        return HandleResult(await Mediator.Send(new GetProfilePhotos.Query { UserId = userId }));
     }
 }
