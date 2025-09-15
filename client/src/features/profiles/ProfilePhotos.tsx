b1b4b7306727dd3bdabfd0037ed7a3818ef08e83
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-15 16:08:33 +0700

    ch17-006 Displaying the images

---
+++ client/src/features/profiles/ProfilePhotos.tsx
@@ -0,0 +1,33 @@
+import { ImageList, ImageListItem, Typography } from "@mui/material";
+import { useProfile } from "../../lib/hooks/useProfile";
+import { useParams } from "react-router";
+
+export default function ProfilePhotos() {
+    const {id} = useParams();
+    const {photos, loadingPhotos} = useProfile(id);
+
+  if(loadingPhotos) return <Typography>Loading photos...</Typography>
+
+  if (!photos || photos.length === 0) return <Typography>No photos</Typography>
+
+  return (
+        <ImageList sx={{ height: 450 }} cols={6} rowHeight={164}>
+            {photos.map((item) => (
+                <ImageListItem key={item.id}>
+                <img
+                    srcSet={`${item.url.replace(
+                        'upload/', 
+                        'upload/w_164,h_164,c_fill,f_auto,dpr_2,g_face/'
+                    )}`}
+                    src={`${item.url.replace(
+                        'upload/', 
+                        'upload/w_164,h_164,c_fill,f_auto,g_face/'                       
+                    )}`}
+                    alt={`user profile image`}
+                    loading="lazy"
+                />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    )
+}
