9d322818897408a9ae0e42a88e93f3c823b07104
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-15 22:49:46 +0700

    ch17-010 Adding React Cropper

--- client/src/app/shared/components/PhotoUploadWidget.tsx
+++ client/src/app/shared/components/PhotoUploadWidget.tsx
@@ -1,11 +1,18 @@
 import { CloudUpload } from "@mui/icons-material";
 import { Box, Grid, Typography } from "@mui/material";
-import { useCallback } from "react";
+import { useCallback, useRef, useState } from "react";
 import {useDropzone} from 'react-dropzone'
+import  { type ReactCropperElement, Cropper } from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
 export default function PhotoUploadWidget() {
+    const [files, setfiles] = useState<File[]>([]);
+    const cropperRef = useRef<ReactCropperElement>(null);
+
     const onDrop = useCallback((acceptedFiles : File[]) => {
-            console.log(acceptedFiles);
+            setfiles(acceptedFiles.map(file => Object.assign(file, {
+                preview: URL.createObjectURL(file)
+            })));
         }, [])
     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
   return (
@@ -29,9 +36,30 @@ export default function PhotoUploadWidget() {
         </Grid>
         <Grid size={4}>
             <Typography variant="overline" color="secondary">Step 2 - Resize image</Typography>
+            {files[0] && 
+                <Cropper 
+                    src = {URL.createObjectURL(files[0])}
+                    style={{height: 300, width: '90%'}}
+                    initialAspectRatio={1}
+                    aspectRatio={1}
+                    preview='.img-preview'
+                    guides={false}
+                    viewMode={1}
+                    background={false}
+                />
+            }
         </Grid>
         <Grid size={4}>
-            <Typography variant="overline" color="secondary">Step 3 - Preview & upload</Typography>
+            {files[0] && 
+                <>
+                     <Typography variant="overline" color="secondary">Step 3 - Preview & upload</Typography>
+                     <div
+                        className="img-preview"
+                        style={{width: '100%', height: 300, overflow: 'hidden'}}
+                     />
+                </>
+            }
+          
         </Grid>
     </Grid>
   )
