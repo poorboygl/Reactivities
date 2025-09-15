5c5545a16d67a8a0723e6c897f1f3bad753011d1
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-15 19:39:46 +0700

    ch17-007 Conditionally rendering the photo upload widget

--- client/src/lib/hooks/useProfile.ts
+++ client/src/lib/hooks/useProfile.ts
@@ -1,13 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
+import { useMemo } from "react";
 
 export const useProfile = (id?: string) => {
+    const queryClient = useQueryClient();
+
     const {data : profile, isLoading : loadingProfile} = useQuery<Profile>({
             queryKey: ['profile', id],
             queryFn:  async () =>{
                 const response = await agent.get<Profile>(`/profiles/${id}`)
                 return  response.data;
-            } 
+            } ,
+            enabled: !!id
     });
 
     const {data: photos, isLoading: loadingPhotos} = useQuery<Photo[]>({
@@ -15,13 +19,19 @@ export const useProfile = (id?: string) => {
             queryFn:  async () =>{
                 const response = await agent.get<Photo[]>(`/profiles/${id}/photos`)
                 return response.data;
-            }   
+            },
+            enabled: !!id
     });
+
+    const isCurrentUser = useMemo(() => {
+        return id === queryClient.getQueryData<User>(['user'])?.id;
+    }, [id, queryClient]);
 
         return {
             profile, 
             loadingProfile,
             photos,
-            loadingPhotos
+            loadingPhotos,
+            isCurrentUser
     };
 }
