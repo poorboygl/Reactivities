01c8a0e618461f1bc029918466a28e9704814406
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-15 13:08:19 +0700

    ch17-005 Getting the profile data

--- client/src/features/profiles/ProfileHeader.tsx
+++ client/src/features/profiles/ProfileHeader.tsx
@@ -1,15 +1,23 @@
 import { Avatar, Box, Button, Chip, Divider, Grid, Paper, Stack, Typography } from "@mui/material";
 
-export default function ProfileHeader() {
+type Props = {
+    profile: Profile;
+}
+
+export default function ProfileHeader({ profile }: Props) {
     const isFollowing = true;
   return (
     <Paper elevation={3} sx={{ p:4, borderRadius: 3}}>
         <Grid container spacing={2}>
             <Grid size={8}>
                 <Stack direction={'row'} spacing={3} alignItems='center'>
-                    <Avatar sx={{ width: 150, height: 150 }}/>
+                    <Avatar 
+                        sx={{ width: 150, height: 150 }} 
+                        alt = {profile.displayName + `image`}
+                        src={profile.imageUrl } 
+                    />
                     <Box display='flex' flexDirection='column' gap={2}>
-                        <Typography variant='h4'>Display name</Typography>
+                        <Typography variant='h4'>{profile.displayName}</Typography>
                         {isFollowing 
                             && <Chip  
                                 variant="outlined" 
--- client/src/features/profiles/ProfilePage.tsx
+++ client/src/features/profiles/ProfilePage.tsx
@@ -1,12 +1,21 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import ProfileHeader from "./ProfileHeader";
 import ProfileContent from "./ProfileContent";
+import { useParams } from "react-router";
+import { useProfile } from "../../lib/hooks/useProfile";
 
 export default function ProfilePage() {
+  const {id} = useParams();
+
+  const {profile, loadingProfile} = useProfile(id);
+
+  if(loadingProfile) return <Typography>Loading Profile...</Typography>
+
+  if(!profile) return <Typography>Profile Not Found</Typography>
   return (
     <Grid container>
         <Grid size={12} >
-            <ProfileHeader />
+            <ProfileHeader profile={profile} />
             <ProfileContent />
         </Grid>
     </Grid>
