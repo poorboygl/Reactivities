ccd78e569186bef656af1ceed3d1444b0d1d99ad
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-16 14:54:51 +0700

    Fix the Bug

--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -1,14 +1,13 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import type { loginSchema } from "../schemas/loginSchema"
 import agent from "../api/agent"
-import { useLocation, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import type { RegisterSchema } from "../schemas/registerSchema";
 import { toast } from "react-toastify";
 
 export const useAccount = () => {
     const queryClient = useQueryClient();
     const navigate = useNavigate();
-    const location = useLocation();
 
     const loginUser = useMutation({
         mutationFn: async (creds: loginSchema) => {
@@ -53,8 +52,6 @@ export const useAccount = () => {
             return response.data;
         },
         enabled: !queryClient.getQueryData(['user']) 
-                    && location.pathname !== '/login'
-                    && location.pathname !== '/register'
     })
 
     return {
