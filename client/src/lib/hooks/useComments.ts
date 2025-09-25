41a5c44968263d1db942c9aede0af607b301f455
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-26 06:44:25 +0700

    ch19-008 Client SignalR setup part 2

--- client/src/lib/hooks/useComments.ts
+++ client/src/lib/hooks/useComments.ts
@@ -1,23 +1,33 @@
 import { useLocalObservable } from "mobx-react-lite"
 import {HubConnection, HubConnectionBuilder, HubConnectionState} from '@microsoft/signalr'
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export const useComments = (activityId?: string) => {
+    const created = useRef(false);
     const commentStore = useLocalObservable(() => ({
+        comments: [] as ChatComment[],
         hubConnection: null as HubConnection | null,
 
         createHubConnection(activityId: string) {
             if(!activityId) return;
             
             this.hubConnection = new HubConnectionBuilder()
-                            .withUrl(`${import.meta.env.VITE_COMMENT_URL}?=${activityId}`,{
+                            .withUrl(`${import.meta.env.VITE_COMMENT_URL}?activityId=${activityId}`,{
                                 withCredentials: true
                             })
                             .withAutomaticReconnect()
                             .build();
 
             this.hubConnection.start()
-                .catch(error => console.log('Error establishing connection: ', error));            
+                .catch(error => console.log('Error establishing connection: ', error));
+                
+            this.hubConnection.on('LoadComments', comments => {
+                this.comments = comments
+            })
+
+            this.hubConnection.on('ReceiveComment', comment => {
+                this.comments.unshift(comment);
+            })
         },
 
         stopHubConnection(){
@@ -30,12 +40,14 @@ export const useComments = (activityId?: string) => {
     }));
 
     useEffect(() => {
-        if(activityId) {
+        if(activityId && !created.current) {
             commentStore.createHubConnection(activityId)
+            created.current = true;
         }
 
         return () => {
             commentStore.stopHubConnection();
+            commentStore.comments = [];
         }
     }, [activityId, commentStore])
 
