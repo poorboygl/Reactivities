e783abcaae469fe55c87693931e28902b47f8353
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-06 21:02:15 +0700

    ch21-007 Adding infinite scrolling

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,22 +1,13 @@
-import {Button, Grid} from '@mui/material'
+import {Grid} from '@mui/material'
 import ActivityList from './ActivityList';
 import ActivityFilters from './ActivityFilters';
-import { useActivities } from '../../../lib/hooks/useActivities';
+
 
 export default function ActivityDashboard() {
-  const {isFetchingNextPage, fetchNextPage, hasNextPage} = useActivities();
   return (
     <Grid container spacing={3}>
       <Grid size={8}>
         <ActivityList />
-        <Button
-          onClick={() => fetchNextPage()}
-          sx={{ my: 2, float:'right'}}
-          variant='contained'
-          disabled = {!hasNextPage || isFetchingNextPage}
-        >
-          Load more
-        </Button>
       </Grid>
       <Grid 
         size ={4}
